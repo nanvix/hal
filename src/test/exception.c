29539c0443db4b1a8506bd60184cd62fff73002a
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-03-22 10:46:23 +0100

    Feature: Unit Tests for Exception Interface

--- src/test/exception.c
+++ src/test/exception.c
@@ -25,6 +25,7 @@
 #include <nanvix/hal/hal.h>
 #include <nanvix/const.h>
 #include <nanvix/klib.h>
+#include <errno.h>
 #include "test.h"
 
 /**
@@ -32,12 +33,41 @@
  */
 #define TEST_EXCEPTION_DESTRUCTIVE 0
 
+/**
+ * @brief Dummy exception handler
+ */
+PRIVATE void dummy_handler(
+	const struct exception *excp,
+	const struct context *ctx
+)
+{
+	UNUSED(excp);
+	UNUSED(ctx);
+}
+
 /*============================================================================*
  * API Tests                                                                  *
  *============================================================================*/
 
+/*----------------------------------------------------------------------------*
+ * Set and Unset an Exception Handler                                         *
+ *----------------------------------------------------------------------------*/
+
 /**
- * @brief API Test: Trigger Exception
+ * API Test: Set and Unset and Exception Handler
+ */
+PRIVATE void test_exception_set_unset_handler(void)
+{
+	KASSERT(exception_set_handler(EXCEPTION_PAGE_FAULT, dummy_handler) == 0);
+	KASSERT(exception_unset_handler(EXCEPTION_PAGE_FAULT) == 0);
+}
+
+/*----------------------------------------------------------------------------*
+ * Trigger an Exception                                                       *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * @brief API Test: Trigger an Exception
  */
 PRIVATE void test_trigger_exception(void)
 {
@@ -50,17 +80,93 @@ PRIVATE void test_trigger_exception(void)
 	*p = 1;
 }
 
-/*============================================================================*
- * Test Driver                                                                *
- *============================================================================*/
+/*----------------------------------------------------------------------------*
+ * Test Driver Table                                                          *
+ *----------------------------------------------------------------------------*/
 
 /**
  * @brief Unit tests.
  */
 PRIVATE struct test exception_api_tests[] = {
-	{ test_trigger_exception, "Trigger Exception" },
-	{ NULL,                   NULL                },
+	{ test_exception_set_unset_handler, "set and unset exception handler" },
+	{ test_trigger_exception,           "trigger exception"               },
+	{ NULL,                              NULL                             },
 };
+
+/*============================================================================*
+ * Fault Injection Tests                                                      *
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*
+ * Set a Handler for Invalid Exception                                        *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Fault Injection test: Set a Handler for an Invalid Exception
+ */
+PRIVATE void test_exception_set_handler_inval(void)
+{
+	KASSERT(exception_set_handler(-1, dummy_handler) == -EINVAL);
+	KASSERT(exception_set_handler(HAL_NUM_EXCEPTIONS + 1, dummy_handler) == -EINVAL);
+}
+
+/*----------------------------------------------------------------------------*
+ * Set a Handler for a Bad Exception                                          *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Fault Injection test: Set a Handler for a Bad Exception
+ */
+PRIVATE void test_exception_set_handler_bad(void)
+{
+	KASSERT(exception_set_handler(EXCEPTION_PAGE_FAULT, dummy_handler) == 0);
+	KASSERT(exception_set_handler(EXCEPTION_PAGE_FAULT, dummy_handler) == -EBUSY);
+	KASSERT(exception_unset_handler(EXCEPTION_PAGE_FAULT) == 0);
+}
+
+/*----------------------------------------------------------------------------*
+ * Unset a Handler for Invalid Exception                                      *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Fault Injection test: Unset a Handler for an Invalid Exception
+ */
+PRIVATE void test_exception_unset_handler_inval(void)
+{
+	KASSERT(exception_unset_handler(-1) == -EINVAL);
+	KASSERT(exception_unset_handler(HAL_NUM_EXCEPTIONS + 1) == -EINVAL);
+}
+
+/*----------------------------------------------------------------------------*
+ * Unset a Handler for a Bad Exception                                        *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Fault Injection test: Unset a Handler for a Bad Exception
+ */
+PRIVATE void test_exception_unset_handler_bad(void)
+{
+	KASSERT(exception_unset_handler(EXCEPTION_PAGE_FAULT) == -EINVAL);
+}
+
+/*----------------------------------------------------------------------------*
+ * Test Driver Table                                                          *
+ *----------------------------------------------------------------------------*/
+
+/**
+ * @brief Unit tests.
+ */
+PRIVATE struct test exception_fault_tests[] = {
+	{ test_exception_set_handler_inval,   "set handler for invalid exception"   },
+	{ test_exception_set_handler_bad,     "set handler for bad exception"       },
+	{ test_exception_unset_handler_inval, "unset handler for invalid exception" },
+	{ test_exception_unset_handler_bad,   "unset handler for bad exception"     },
+	{ NULL,                                NULL                                 },
+};
+
+/*============================================================================*
+ * Test Driver                                                                *
+ *============================================================================*/
 
 /**
  * The test_exception() function launches testing units on the
@@ -73,7 +179,13 @@ PUBLIC void test_exception(void)
 	for (int i = 0; exception_api_tests[i].test_fn != NULL; i++)
 	{
 		exception_api_tests[i].test_fn();
-		kprintf("[test][stress][clock] %s [passed]", exception_api_tests[i].name);
+		kprintf("[test][api][exception] %s [passed]", exception_api_tests[i].name);
+	}
+
+	for (int i = 0; exception_fault_tests[i].test_fn != NULL; i++)
+	{
+		exception_fault_tests[i].test_fn();
+		kprintf("[test][fault][exception] %s [passed]", exception_fault_tests[i].name);
 	}
 }
 
