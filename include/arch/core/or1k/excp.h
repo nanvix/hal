baaa8190394c0fc655ef54c7ac3784e9e5e35e43
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-04-10 13:27:27 +0200

    [or1k] Enhancement: Exception Interface

--- include/arch/core/or1k/excp.h
+++ include/arch/core/or1k/excp.h
@@ -25,6 +25,11 @@
 #ifndef ARCH_CORE_OR1K_EXCP_H_
 #define ARCH_CORE_OR1K_EXCP_H_
 
+	/* Must come first. */
+	#define __NEED_CORE_TYPES
+	#define __NEED_MEMORY_TYPES
+	#define __NEED_OR1K_CONTEXT
+
 /**
  * @addtogroup or1k-core-exception Exception
  * @ingroup or1k-core
@@ -33,23 +38,15 @@
  */
 /**@{*/
 
-	#define __NEED_OR1K_CONTEXT
 	#include <arch/core/or1k/context.h>
-
-#ifndef _ASM_FILE_
-
-	#define __NEED_MEMORY_TYPES
 	#include <arch/core/or1k/types.h>
-
 	#include <nanvix/const.h>
-	#include <stdint.h>
-
-#endif /* _ASM_FILE_ */
+	#include <nanvix/klib.h>
 
 	/**
 	 * @brief Exception information size (in bytes).
 	 */
-	#define OR1K_EXCEPTION_SIZE 16
+	#define OR1K_EXCP_SIZE 16
 
 	/**
 	 * @name Offsets to the Exception Information structure.
@@ -57,56 +54,68 @@
 	 * @see exception
 	 */
 	/**@{*/
-	#define OR1K_EXCEPTION_NUM   0 /**< Exception Number      */
-	#define OR1K_EXCEPTION_EEAR  4 /**< Exception Address     */
-	#define OR1K_EXCEPTION_EPCR  8 /**< Saved Program Counter */
+	#define OR1K_EXCP_NR    0 /**< Exception Number      */
+	#define OR1K_EXCP_EEAR  4 /**< Exception Address     */
+	#define OR1K_EXCP_EPCR  8 /**< Saved Program Counter */
 	/**@}*/
 
 	/**
 	 * @brief Number of exceptions in the or1k core.
 	 */
-	#define OR1K_NUM_EXCEPTIONS 10
+	#define OR1K_EXCP_NUM 10
 
 	/**
 	 * @name or1k Exception Codes
 	 */
 	/**@{*/
-	#define OR1K_EXCEPTION_RESET                 0 /**@< Reset exception          */
-	#define OR1K_EXCEPTION_BUSERROR              1 /**@< Bus Error Exception      */
-	#define OR1K_EXCEPTION_PAGE_FAULT            2 /**@< Page fault Exception     */
-	#define OR1K_EXCEPTION_ALIGNMENT             3 /**@< Alignment Exception      */
-	#define OR1K_EXCEPTION_ILLEGAL_INSTRUCTION   4 /**@< Illegal Instruction      */
-	#define OR1K_EXCEPTION_DTLB_FAULT            5 /**@< Data TLB Fault           */
-	#define OR1K_EXCEPTION_ITLB_FAULT            6 /**@< Instruction TLB Fault    */
-	#define OR1K_EXCEPTION_RANGE                 7 /**@< Range Exception          */
-	#define OR1K_EXCEPTION_FLOAT_POINT           8 /**@< Floating Point Exception */
-	#define OR1K_EXCEPTION_TRAP                  9 /**@< Trap Exception           */
+	#define OR1K_EXCP_RESET                 0 /**@< Reset exception          */
+	#define OR1K_EXCP_BUSERROR              1 /**@< Bus Error Exception      */
+	#define OR1K_EXCP_PAGE_FAULT            2 /**@< Page fault Exception     */
+	#define OR1K_EXCP_ALIGNMENT             3 /**@< Alignment Exception      */
+	#define OR1K_EXCP_ILLEGAL_INSTRUCTION   4 /**@< Illegal Instruction      */
+	#define OR1K_EXCP_DTLB_FAULT            5 /**@< Data TLB Fault           */
+	#define OR1K_EXCP_ITLB_FAULT            6 /**@< Instruction TLB Fault    */
+	#define OR1K_EXCP_RANGE                 7 /**@< Range Exception          */
+	#define OR1K_EXCP_FLOAT_POINT           8 /**@< Floating Point Exception */
+	#define OR1K_EXCP_TRAP                  9 /**@< Trap Exception           */
 	/**@}*/
 
 #ifndef _ASM_FILE_
 
-/**
- * @cond or1k
- */
-
 	/**
-	 * @brief Exception information.
+	 * @cond or1k
 	 */
-	struct exception
-	{
-		or1k_word_t num;         /**< Exception number.      */
-		or1k_word_t eear;        /**< Exception address.     */
-		or1k_word_t epcr;        /**< Saved program counter. */
-		or1k_byte_t RESERVED[4]; /**< Required padding.      */
 
-	} __attribute__((packed));
+		/**
+		 * @brief Exception information.
+		 */
+		struct exception
+		{
+			or1k_word_t num;         /**< Exception number.      */
+			or1k_word_t eear;        /**< Exception address.     */
+			or1k_word_t epcr;        /**< Saved program counter. */
+			or1k_byte_t RESERVED[4]; /**< Required padding.      */
 
-/**@endcond*/
+		} PACK;
+
+	/**@endcond*/
 
 	/**
 	 * @brief Exception handler.
 	 */
-	typedef void (*or1k_exception_handler_fn)(const struct exception *, const struct context *);
+	typedef void (*or1k_excp_handler_fn)(const struct exception *, const struct context *);
+
+	/**
+	 * @brief Dumps information about an exception.
+	 *
+	 * @param excp Exception information.
+	 */
+	EXTERN void or1k_excp_dump(const struct exception *excp);
+
+	/**
+	 * @brief Low-level exception dispatcher.
+	 */
+	EXTERN void _or1k_do_excp(void);
 
 	/**
 	 * @brief Gets the number of an exception.
@@ -165,42 +174,6 @@
 		return (excp->epcr);
 	}
 
-	/**
-	 * @brief Sets a handler for an exception.
-	 *
-	 * @param num     Number of the target exception.
-	 * @param handler Exception handler.
-	 *
-	 * @returns Upon successful completion zero is returned. Upon
-	 * failure a negative error code is returned instead.
-	 */
-	EXTERN int or1k_excp_set_handler(int num, or1k_exception_handler_fn handler);
-
-	/**
-	 * @brief Unsets a handler for an exception.
-	 *
-	 * @param num Number of the target exception.
-	 *
-	 * @returns Upon successful completion zero is returned. Upon
-	 * failure a negative error code is returned instead.
-	 */
-	EXTERN int or1k_excp_unset_handler(int num);
-
-	/**
-	 * @brief Low-level exception dispatcher.
-	 */
-	EXTERN void _do_excp(void);
-
-	/**
-	 * @brief High-level exception dispatcher.
-	 *
-	 * @brief excp Exception information.
-	 * @brief ctx  Saved execution context.
-	 *
-	 * @note This function is called from assembly code.
-	 */
-	EXTERN void do_excp(const struct exception *excp, const struct context *ctx);
-
 #endif /* _ASM_FILE_ */
 
 /**@}*/
@@ -213,37 +186,42 @@
  * @cond or1k
  */
 
-
 	/**
 	 * @name Exported Constants
 	 */
 	/**@{*/
-	#define EXCEPTIONS_NUM                OR1K_NUM_EXCEPTIONS                /**< @ref OR1K_NUM_EXCEPTIONS                */
-	#define EXCEPTION_SIZE                OR1K_EXCEPTION_SIZE                /**< @ref OR1K_EXCEPTION_SIZE                */
-	#define EXCEPTION_INVALID_OPCODE      OR1K_EXCEPTION_ILLEGAL_INSTRUCTION /**< @ref OR1K_EXCEPTION_ILLEGAL_INSTRUCTION */
-	#define EXCEPTION_PAGE_FAULT          OR1K_EXCEPTION_PAGE_FAULT          /**< @ref OR1K_EXCEPTION_PAGE_FAULT          */
-	#define EXCEPTION_PAGE_PROTECTION     OR1K_EXCEPTION_PAGE_FAULT          /**< @ref OR1K_EXCEPTION_PAGE_FAULT          */
-	#define EXCEPTION_ITLB_FAULT          OR1K_EXCEPTION_ITLB_FAULT          /**< @ref OR1K_EXCEPTION_ITLB_FAULT          */
-	#define EXCEPTION_DTLB_FAULT          OR1K_EXCEPTION_DTLB_FAULT          /**< @ref OR1K_EXCEPTION_DTLB_FAULT          */
-	#define EXCEPTION_GENERAL_PROTECTION  OR1K_EXCEPTION_RESET               /**< @ref OR1K_EXCEPTION_RESET               */
+	#define EXCEPTIONS_NUM                OR1K_EXCP_NUM                 /**< @ref OR1K_EXCP_NUMS                */
+	#define EXCEPTION_SIZE                OR1K_EXCP_SIZE                /**< @ref OR1K_EXCP_SIZE                */
+	#define EXCEPTION_INVALID_OPCODE      OR1K_EXCP_ILLEGAL_INSTRUCTION /**< @ref OR1K_EXCP_ILLEGAL_INSTRUCTION */
+	#define EXCEPTION_PAGE_FAULT          OR1K_EXCP_PAGE_FAULT          /**< @ref OR1K_EXCP_PAGE_FAULT          */
+	#define EXCEPTION_PAGE_PROTECTION     OR1K_EXCP_PAGE_FAULT          /**< @ref OR1K_EXCP_PAGE_FAULT          */
+	#define EXCEPTION_ITLB_FAULT          OR1K_EXCP_ITLB_FAULT          /**< @ref OR1K_EXCP_ITLB_FAULT          */
+	#define EXCEPTION_DTLB_FAULT          OR1K_EXCP_DTLB_FAULT          /**< @ref OR1K_EXCP_DTLB_FAULT          */
+	#define EXCEPTION_GENERAL_PROTECTION  OR1K_EXCP_RESET               /**< @ref OR1K_EXCP_RESET               */
 	/**@}*/
 
 	/**
 	 * @name Exported Structures
 	 */
 	/**@{*/
-	#define __exception_struct      /**< @ref exception */
+	#define __exception_struct /**< @ref exception */
 	/**@}*/
 
 	/**
 	 * @name Exported Functions
 	 */
 	/**@{*/
-	#define __exception_get_addr      /**< @ref exception_get_addr()      */
-	#define __exception_get_instr     /**< @ref exception_get_instr()     */
-	#define __exception_get_num       /**< @ref exception_get_num()       */
-	#define __exception_set_handler   /**< @ref exception_set_handler()   */
-	#define __exception_unset_handler /**< @ref exception_unset_handler() */
+	#define __exception_get_addr_fn  /**< @ref exception_get_addr()  */
+	#define __exception_get_instr_fn /**< @ref exception_get_instr() */
+	#define __exception_get_num_fn   /**< @ref exception_get_num()   */
+	#define __exception_dump_fn      /**< @ref exception_dump()      */
+	/**@}*/
+
+	/**
+	 * @name Exported Variables
+	 */
+	/**@{*/
+	#define __exceptions_var /**< @ref exceptions */
 	/**@}*/
 
 #ifndef _ASM_FILE_
@@ -273,29 +251,12 @@
 	}
 
 	/**
-	 * @see or1k_excp_set_handler().
+	 * @see or1k_excp_dump().
 	 */
-	static inline int exception_set_handler(int num, or1k_exception_handler_fn handler)
+	static inline void exception_dump(const struct exception *excp)
 	{
-		return (or1k_excp_set_handler(num, handler));
+		or1k_excp_dump(excp);
 	}
-
-	/**
-	 * @see or1k_excp_unset_handler().
-	 */
-	static inline int exception_unset_handler(int num)
-	{
-		return (or1k_excp_unset_handler(num));
-	}
-
-	/**
-	 * @brief Forwards an exception.
-	 *
-	 * @param num  Target exception.
-	 * @param excp Exception to be forwarded.
-	 * @param ctx  Context information of the forwarded exception.
-	 */
-	EXTERN void forward_excp(int num, const struct exception *excp, const struct context *ctx);
 
 #endif /* _ASM_FILE_ */
 
--- src/hal/arch/core/or1k/ctx.c
+++ src/hal/arch/core/or1k/ctx.c
@@ -22,34 +22,27 @@
  * SOFTWARE.
  */
 
-#include <nanvix/hal/core/exception.h>
+#include <arch/core/or1k/context.h>
 #include <nanvix/const.h>
 #include <nanvix/klib.h>
 
 /**
- * @cond or1k
- *
- * Lookup table with information about exceptions.
- *
- * @endcond
- */
-PUBLIC struct exception_info exceptions[OR1K_EXCP_NUM] = {
-	{ NULL, "reset exception"           },
-	{ NULL, "bus error"                 },
-	{ NULL, "page fault"                },
-	{ NULL, "alignment check exception" },
-	{ NULL, "illegal instruction"       },
-	{ NULL, "data tlb fault"            },
-	{ NULL, "instruction tlb fault"     },
-	{ NULL, "data out of range"         },
-	{ NULL, "float point exception"     },
-	{ NULL, "trap exception"            }
-};
-
-/**
  * @todo TODO provide a detailed description for this function.
+ *
+ * @author Pedro Henrique Penna
  */
-PUBLIC void or1k_excp_dump(const struct exception *excp)
+PUBLIC void or1k_context_dump(const struct context *ctx)
 {
-	kprintf("%s", exceptions[excp->num].name);
+	/* Dump general purpose registers. */
+	kprintf("[or1k]  r0=%x  r1=%x  r2=%x  r3=%x", ctx->r0,  ctx->r1,  ctx->r2,  ctx->r3);
+	kprintf("[or1k]  r4=%x  r5=%x  r6=%x  r7=%x", ctx->r4,  ctx->r5,  ctx->r6,  ctx->r7);
+	kprintf("[or1k]  r8=%x  r9=%x r10=%x r11=%x", ctx->r8,  ctx->r9,  ctx->r10, ctx->r11);
+	kprintf("[or1k] r12=%x r13=%x r14=%x r15=%x", ctx->r12, ctx->r13, ctx->r14, ctx->r15);
+	kprintf("[or1k] r16=%x r17=%x r18=%x r19=%x", ctx->r16, ctx->r17, ctx->r18, ctx->r19);
+	kprintf("[or1k] r20=%x r21=%x r22=%x r23=%x", ctx->r20, ctx->r21, ctx->r22, ctx->r23);
+	kprintf("[or1k] r24=%x r25=%x r26=%x r27=%x", ctx->r24, ctx->r25, ctx->r26, ctx->r27);
+	kprintf("[or1k] r28=%x r29=%x r30=%x r31=%x", ctx->r28, ctx->r29, ctx->r30, ctx->r31);
+
+	/* Dump special function registers. */
+	kprintf("[or1k] epcr=%x  eear=%x  esr=%x", ctx->epcr, ctx->eear, ctx->esr);
 }
