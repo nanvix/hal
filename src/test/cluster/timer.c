2c85eaf132fa612d2ddfe63fa32ca1fed2d44f57
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-06-17 07:37:19 +0200

    Feature: Unit Tests for Timer Interface

---
+++ src/test/cluster/timer.c
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2019 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <nanvix/hal/hal.h>
+#include <nanvix/const.h>
+#include <nanvix/klib.h>
+#include "../test.h"
+
+/**
+ * @brief Launch verbose tests?
+ */
+#define TEST_TIMER_VERBOSE 0
+
+/*============================================================================*
+ * Test Driver                                                                *
+ *============================================================================*/
+
+/**
+ * The test_spinlock() function launches testing units on the spinlock
+ * interface of the HAL.
+ *
+ * @author Daniel Coscia
+ */
+PUBLIC void test_timer(void)
+{
+#ifndef __x86__
+    timer_init(0);
+    kprintf("clock = %d", clock_read());
+#endif
+}
