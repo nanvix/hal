e4dbb38f4666652a849c01aa2edbda4c4e3ceee1
João Vicente Souto <joaovicentesouto@gmail.com>
2020-08-20 11:00:58 -0300

    [k1b-core] Enhancement: Drop the dependency on BSP and use our interrupt dispatcher.

--- src/hal/arch/core/k1b/ivt.c
+++ src/hal/arch/core/k1b/ivt.c
@@ -37,62 +37,22 @@
 /**
  * @brief High-level interrupt dispatcher.
  */
-PRIVATE k1b_int_handler_fn _k1b_do_int = NULL;
-
-/**
- * @brief Interrupt vectors.
- */
-PRIVATE struct {
-	int intnum; /**< Interrupt Number          */
-	int hwint;  /**< Hardware Interrupt Number */
-} hwints[K1B_IVT_LENGTH] = {
-	{ BSP_IT_TIMER_0, K1B_INT_TIMER0    }, /* Timer 0              */
-	{ BSP_IT_TIMER_1, K1B_INT_TIMER1    }, /* Timer 1              */
-	{ BSP_IT_WDOG,    K1B_INT_TIMER     }, /* Watchdog Timer       */
-	{ BSP_IT_CN,      K1B_INT_CNOC      }, /* Control NoC          */
-	{ BSP_IT_RX,      K1B_INT_DNOC      }, /* Data NoC             */
-	{ BSP_IT_UC,      K1B_INT_DMA       }, /* DMA                  */
-	{ BSP_IT_NE,      K1B_INT_NOC_ERR   }, /* NoC Error            */
-	{ BSP_IT_WDOG_U,  K1B_INT_TIMER_ERR }, /* Watchdog Timer Error */
-	{ BSP_IT_PE_0,    K1B_INT_VIRT      }, /* Remote Core 0        */
-	{ BSP_IT_PE_1,    K1B_INT_VIRT      }, /* Remote Core 1        */
-	{ BSP_IT_PE_2,    K1B_INT_VIRT      }, /* Remote Core 2        */
-	{ BSP_IT_PE_3,    K1B_INT_VIRT      }, /* Remote Core 3        */
-	{ BSP_IT_PE_4,    K1B_INT_VIRT      }, /* Remote Core 4        */
-	{ BSP_IT_PE_5,    K1B_INT_VIRT      }, /* Remote Core 5        */
-	{ BSP_IT_PE_6,    K1B_INT_VIRT      }, /* Remote Core 6        */
-	{ BSP_IT_PE_7,    K1B_INT_VIRT      }, /* Remote Core 7        */
-	{ BSP_IT_PE_8,    K1B_INT_VIRT      }, /* Remote Core 8        */
-	{ BSP_IT_PE_9,    K1B_INT_VIRT      }, /* Remote Core 9        */
-	{ BSP_IT_PE_10,   K1B_INT_VIRT      }, /* Remote Core 10       */
-	{ BSP_IT_PE_11,   K1B_INT_VIRT      }, /* Remote Core 11       */
-	{ BSP_IT_PE_12,   K1B_INT_VIRT      }, /* Remote Core 12       */
-	{ BSP_IT_PE_13,   K1B_INT_VIRT      }, /* Remote Core 14       */
-	{ BSP_IT_PE_14,   K1B_INT_VIRT      }, /* Remote Core 14       */
-	{ BSP_IT_PE_15,   K1B_INT_VIRT      }, /* Remote Core 15       */
-};
+PRIVATE k1b_int_handler_fn _do_interrupt = NULL;
 
 /**
  * @brief Low-level interrupt dispatcher.
  *
- * @param intnum Interrupt number.
- * @param ctx    Saved interrupted context.
+ * @param hwint Interrupt number.
+ * @param ctx   Saved interrupted context.
  *
  * @author Pedro Henrique Penna
  */
-PRIVATE void __k1b_do_int(int intnum, __k1_vcontext_t *ctx)
+PUBLIC void __k1b_do_int(int hwint, struct context * ctx)
 {
 	UNUSED(ctx);
 
-	for (int i = 0; i < K1B_IVT_LENGTH; i++)
-	{
-		if (hwints[i].intnum == intnum)
-		{
-			if (LIKELY(_k1b_do_int != NULL))
-				_k1b_do_int(hwints[i].hwint);
-			return;
-		}
-	}
+	if (LIKELY(_do_interrupt != NULL))
+		_do_interrupt(hwint);
 }
 
 /**
@@ -113,14 +73,16 @@ PRIVATE void k1b_ivt_setup(
 	k1b_int_handler_fn hwint_handler,
 	k1b_swint_handler_fn swint_handler,
 	void (*excp_handler)(void),
-	void *stack)
+	void (*it_handler)(void),
+	void *stack
+)
 {
 	kprintf("[hal] exception stack at %x", stack);
 
 	/* Register dispatchers. */
-	_k1b_do_int = hwint_handler;
-	for (int i = 0; i < K1B_IVT_LENGTH; i++)
-		bsp_register_it(__k1b_do_int, hwints[i].intnum);
+	_do_interrupt = hwint_handler;
+	mOS_register_it_handler(it_handler);
+
 	mOS_register_scall_handler(swint_handler);
 	mOS_register_trap_handler(excp_handler);
 	k1b_dcache_inval();
@@ -137,12 +99,14 @@ PRIVATE void k1b_ivt_setup(
  *
  * @author Pedro Henrique Penna
  */
-PUBLIC void ivt_setup(void *stack)
+PUBLIC void ivt_setup(void * stack)
 {
 	k1b_ivt_setup(
 		do_interrupt,
 		_k1b_do_kcall,
 		_k1b_do_excp,
+		_k1b_do_int,
 		stack
 	);
 }
+
